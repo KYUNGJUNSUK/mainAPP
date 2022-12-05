26c35b2916b78d29a9d57bcf8948e6e5d5ec9b77
KYUNGJUNSUK <kptyujim@sungkyul.ac.kr>
2022-12-05 22:27:03 +0900

    주석

---
+++ reservation/login.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- SEO Meta Tags -->
+    <meta name="description" content="Create a stylish landing page for your business startup and get leads for the offered services with this HTML landing page template.">
+    <meta name="author" content="Inovatik">
+
+    <!-- OG Meta Tags to improve the way the post looks when you share the page on LinkedIn, Facebook, Google+ -->
+	<meta property="og:site_name" content="" /> <!-- website name -->
+	<meta property="og:site" content="" /> <!-- website link -->
+	<meta property="og:title" content=""/> <!-- title shown in the actual shared post -->
+	<meta property="og:description" content="" /> <!-- description shown in the actual shared post -->
+	<meta property="og:image" content="" /> <!-- image link, make sure it's jpg -->
+	<meta property="og:url" content="" /> <!-- where do you want your post to link to -->
+	<meta property="og:type" content="article" />
+
+    <!-- Website Title -->
+    <title>LOGIN</title>
+    
+    <!-- Styles -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/fontawesome-all.css" rel="stylesheet">
+    <link href="css/swiper.css" rel="stylesheet">
+	<link href="css/magnific-popup.css" rel="stylesheet">
+	<link href="css/styles.css" rel="stylesheet">
+    <iframe src="34.64.255.232/앱프과제/reservation/login.php" ></iframe>
+    <p>성결 식당 홈페이지에 오신걸 환영합니다!</p><hr><br><br><br> 
+    <p>로그인을 해주세요!</p>
+	
+</head>
+
+<body>
+
+    <form action="loginCheck.php" method="post" enctype="multipart/form-data">
+        
+        <table width = "350" height="70">
+            <tr>
+                <td><label >ID</label></td>
+                <td><input style="width:170px;height:30;" type="text" name="id"></td>
+            </tr>
+            <tr>
+                <td><label >Password</label></td> 
+                <td><input style="width:170px;height:30;" type="text" name="pw"></td>
+            </tr>
+        </table>
+        <br><br>
+        <button type="submit">로그인</button> 
+        <button type="button" onclick="location.href='member_create.html'">회원가입</button>
+        
+    </form>
+                               
+</body>
+</html>
